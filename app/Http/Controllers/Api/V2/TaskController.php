f4bd4386ba4460d92b084bc99f2dfba02c1f0517
maitalata <maitalata@gmail.com>
2024-07-09 14:57:37 +0100

    Update

---
+++ app/Http/Controllers/Api/V2/TaskController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers\Api\V2;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreTaskRequest;
+use App\Http\Requests\UpdateTaskRequest;
+use App\Http\Resources\TaskResource;
+use App\Models\Task;
+use Illuminate\Support\Facades\Gate;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        Gate::authorize('viewAny', Task::class);
+        return TaskResource::collection(auth()->user()->tasks()->latest()->get());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    // public function create()
+    // {
+
+    // }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTaskRequest $request)
+    {
+        if (request()->user()->cannot('create', Task::class)) {
+            abort(403, 'You are not allowed to update this task.');
+        } 
+
+        $task = $request->user()->tasks()->create($request->validated());
+
+        return TaskResource::make($task);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Task $task)
+    {
+        Gate::authorize('view', $task);
+        return TaskResource::make($task);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    // public function edit(Task $task)
+    // {
+
+    // }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTaskRequest $request, Task $task)
+    {
+        if ($request->user()->cannot('update', $task)) {
+            abort(403, 'You are not allowed to update this task.');
+        }
+
+        $task->update($request->validated());
+
+        return TaskResource::make($task);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Task $task)
+    {
+        if (request()->user()->cannot('delete', $task)) {
+            abort(403, 'You are not allowed to update this task.');
+        }
+
+        $task->delete();
+
+        return response()->noContent();
+    }
+}
---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(LoginRequest $request)
+    {
+        // $user = User::where('email', $request->email)->first();
+
+        // if (! $user || ! Hash::check($request->password, $user->password)) {
+        //     throw ValidationException::withMessages(
+        //         [
+        //         '   email' => ['The provided credentials are incorrect.'],
+        //          ]
+        //     );
+        // }
+
+        if (!auth()->attempt($request->only('email', 'password'))) {
+            throw ValidationException::withMessages(
+                [
+                '   email' => ['The provided credentials are incorrect.'],
+                 ]
+            );
+        }
+
+        
+    }
+}
