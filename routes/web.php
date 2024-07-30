f608d1e1a83eef067300f02c35b3b8c869c68f25
Umar Sunusi Maitalata <maitalata@gmail.com>
2024-07-30 03:38:02 +0100

    Removed Laravel welcome page

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,6 @@ use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 
 Route::prefix('auth')->group(function () {
