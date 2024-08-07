fd16430068ff3c8640d0447be7b3e947e65cdea2
Umar Sunusi Maitalata <maitalata@gmail.com>
2024-08-07 11:55:01 +0100

    Added Tasks Summary

---
+++ app/Http/Controllers/Api/V2/SummaryController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Api\V2;
+
+use App\Http\Resources\TaskSummaryResource;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class SummaryController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        $tasks = $request->user()->tasksSummary();
+
+        return $tasks->mapToGroups(
+            function ($item, $key) {
+                return [
+                    ($item->is_completed ? 'completed' : 'uncompleted') => TaskSummaryResource::make($item),
+                ];
+            }
+        );
+    }
+}
