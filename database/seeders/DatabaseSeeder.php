e1c882f3c080d59ec1155f8a43d996dade919f8c
Umar Sunusi Maitalata <maitalata@gmail.com>
2024-08-10 15:04:56 +0100

    Update on Tasks Summary

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use App\Models\Task;
+use Carbon\Carbon;
+use Carbon\CarbonPeriod;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -16,13 +18,31 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory(5)->has(
-            Task::factory(10)
-        )->create();
+        // User::factory(5)->has(
+        //     Task::factory(10)
+        // )->create();
 
         // User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $start = now()->startOfMonth()->subMonthNoOverflow();
+        $end = now();
+        $period = CarbonPeriod::create($start, '1 day', $end);
+        User::factory(5)->create()->each(
+            function ($user) use ($period) {
+                foreach ($period as $date) {
+                    $date->hour(rand(0, 23))->minute(rand(0, 6) * 10);
+                    Task::factory()->create(
+                        [
+                            'user_id' => $user->id,
+                            'created_at' => $date,
+                            'updated_at' => $date,
+                        ]
+                    );
+                }
+            }
+        );
     }
 }
